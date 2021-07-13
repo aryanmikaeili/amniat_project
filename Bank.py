b299c6dce172bc7d167be8ecc231157e4b6a49b3
mohammadrzaaa <mrahmadkhaniha@ce.sharif.edu>
2021-07-13 23:36:26 +0430

    Update Bank.py
    update bank class, connection remained

--- Bank.py
+++ Bank.py
@@ -1,7 +1,12 @@
+from dataclasses import dataclass
+from getpass import getpass
+from random import randrange
+from secrets import randbelow
+from time import sleep
+from typing import Dict, Tuple, Callable, ClassVar
 import socket
 import ssl
 import time
-
 import pprint
 from datetime import datetime, timedelta
 from cryptography import x509
@@ -19,56 +24,134 @@ HEADERSIZE = 64
 CA_IP = '127.0.0.1'
 CA_PORT = 2500
 
-
-
-
-
-
 def send_message(socket, msg):
     message_len = f"{len(msg):<{HEADERSIZE}}"
     message_len = bytes(message_len, 'utf-8')
     socket.send(message_len)
     socket.send(msg)
 
+class Menu:
+    MENU: ClassVar[Tuple[
+        Tuple[
+            str, Callable[['Menu'], bool]
+        ], ...
+    ]]
+
+    def screen(self):
+        prompt = '\n'.join(
+            f'{i}. {name}'
+            for i, (name, fun) in enumerate(self.MENU)
+        ) + '\n'
+
+        while True:
+            choice = input(prompt)
+
+            try:
+                name, fun = self.MENU[int(choice)]
+            except ValueError:
+                print('Invalid integer entered')
+            except IndexError:
+                print('Choice out of range')
+            else:
+                if fun(self):
+                    break
 
 
+@dataclass
+class Account(Menu):
+    card: str
+    pin: str
+    balance : float
 
+    @classmethod
+    def generate(cls) -> 'Account':
+        return cls(
+            card=f'400000{randrange(1e10):010}',
+            pin=f'{randbelow(10_000):04}',
+            balance=0
+        )
 
-class BankAccount:
-    """
-    BankAccount Class
-    """
-    def __init__(self, id: int):
-        self.id = id
-        self.balance = 0
+    def dump(self):
+        print(
+            f"Your card number: {self.card}\n"
+            f"Your PIN: {self.pin}\n"
+            f"Your balance: {self.balance}"
+        )
 
-    def deposit(self, amount: float) -> bool:
-        '''adds money to balance'''
+    def balance(self):
+        print(self.balance)
+
+    def logout(self) -> bool:
+        print('You have successfully logged out!')
+        return True
+
+    def deposit(self) -> bool:
+        amount = float(input("how much money to deposit?"))
         if amount > 0:
             self.balance += amount
-            return True
+            # return True
         return False
 
-    def withdraw(self, amount: float) -> bool:
-        '''subtracts money from balance if funds are sufficient'''
+    def withdraw(self) -> bool:
+        amount = float(input("how much money to withdraw?"))
         if amount > self.balance or amount < 0:
             return False
         self.balance -= amount
+        # return True
+
+    def exit(self):
+        print('Bye!')
+        exit()
+
+    MENU = (
+        ('Exit', exit),
+        ('Balance', balance),
+        ('Log out', logout),
+        ('deposit', deposit),
+        ('withdraw', withdraw),
+    )
+
+
+class BankingSystem(Menu):
+    def __init__(self):
+        self.accounts: Dict[str, Account] = {}
+
+    def create_account(self):
+        account = Account.generate()
+        print('Your card has been created')
+        account.dump()
+        self.accounts[account.card] = account
+
+    def log_in(self):
+        for _ in range(3):
+            card = input('Enter your card number: ')
+            pin = getpass('Enter your PIN: ')
+
+            account = self.accounts.get(card)
+            if account is None or account.pin != pin:
+                print('Wrong card or PIN')
+                sleep(2)
+            else:
+                print('You have successfully logged in!')
+                account.screen()
+                break
+
+    def exit(self) -> bool:
+        print('Bye!')
         return True
 
-    def display_balance(self):
-        """ displays current account balance """
-        print(f'\nNet Available Balance is ${self.balance}')
 
+    # From here is for crt and connection**********************************************************
     def connect_with_ca(self):
         context = ssl.create_default_context()
         context.verify_mode = ssl.CERT_REQUIRED
-        context.load_verify_locations("ca_cert.crt")
+        context.load_verify_locations("ca_cert.pem")
         sock = socket.create_connection((CA_IP, CA_PORT))
         ssock = context.wrap_socket(sock, server_hostname='CA_SERVER')
 
         print('connected with CA server!')
         return ssock
+
     def make_cert(self, hostname):
         key = rsa.generate_private_key(public_exponent=65537, key_size=2048, backend=default_backend())
         name = x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, hostname)])
@@ -89,6 +172,7 @@ class BankAccount:
         )
         client_pem = cert.public_bytes(serialization.Encoding.DER)
         return client_pem, key
+
     def recv_cert(self, socket):
         message_len = socket.recv(HEADERSIZE).decode('utf-8')
         message_len = int(message_len)
@@ -133,40 +217,34 @@ class BankAccount:
         ssock.close()
         return username, cert_file_name, key_file_name
 
-#
-# def interface():
-#     """
-#     Interface for interacting with the bank account
-#     """
-#     print(f'Hello! Welcome to the Deposit and Withdrawal Machine {id}!')
-#     while True:
-#         action = input('\nWould you like to deposit (D), withdraw (W), show balance (S), or exit (E)? ').upper()
-#
-#         if action in "DWSE":
-#             if action == "D":
-#                 try:
-#                     deposit_amount = float(input("How much would you like to deposit: "))
-#                     if not account.deposit(deposit_amount):
-#                         print("Please enter a positive number!")
-#                     else:
-#                         print(f"Successfully deposited {deposit_amount} into your account.")
-#                 except ValueError:
-#                     print("Please enter a positive number.")
-#             if action == "W":
-#                 try:
-#                     withdraw_amount = float(input("How much would you like to withdraw: "))
-#                     if not account.withdraw(withdraw_amount):
-#                         print("You do not have enough money to withdraw.")
-#                     else:
-#                         print(f"Successfully withdraw {withdraw_amount} from your account.")
-#                 except ValueError:
-#                     print("Please enter a positive number.")
-#             if action == "S":
-#                 account.display_balance()
-#             if action == "E":
-#                 break
+    def set_up_server(self, ip, port):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((ip, port))
+        s.listen(5)
 
-if __name__ == '__main__':
-    name = input('What is your id? ')
-    account = BankAccount(id)
-    interface()
+        context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+
+        context.load_cert_chain(certfile=self.CERT, keyfile=self.KEY)
+
+        self.sll_sock = context.wrap_socket(s, server_side=True)
+        print("We are online!")
+
+        # Until here is for crt and connection**********************************************************
+
+
+
+
+
+
+
+
+
+    MENU = (
+        ('Exit', exit),
+        ('Create an account', create_account),
+        ('Log into an account', log_in),
+    )
+
+
+BankingSystem().screen()
